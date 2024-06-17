e311d851de6ecdca1ba8a67b3b4771210fdf2392
Md Sopon Hossain <mdsoponhossain2388@gmail.com>
2024-06-17 08:37:14 +0600

    stay-nest updated

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-### website link:https://hotel-dote-com.web.app
+### website link:https://stay-nest-3a8c7.web.app
 ## Description:
 This is a hotel booking website. People can book room in a hotel .If anyone book a room , first he/she need to sign UP the website.User can update booking   cancel date before one day.After all the website is eye catching and user friendly.
 
--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -3,7 +3,7 @@
 const ErrorPage = () => {
     return (
         <div className="h-[900px]">
-            <div className="grid justify-center h-2/4 border-4 items-center">
+            <div className="grid justify-center h-2/4 items-center">
                 <span className="text-xl font-bold text-gray-700">This page is not found</span>
                 <img src="https://i.ibb.co/CHKVp6W/error-1.jpg" alt="" />
             </div>
